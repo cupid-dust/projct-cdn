ead4e8305ad650c2bde69cd701d9794e4808fa1e
cupid-dust <ahsanmansoor7572@gmail.com>
2023-05-17 10:46:23 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+function ChatWidget() {
+  const handleClick = () => {
+    // Handle chat widget toggle or open action
+    console.log("Chat widget is toggled or opened");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chat-widget">
+      <button className="chat-widget-button" onClick={handleClick}>
+        Chat
+      </button>
     </div>
   );
 }
 
-export default App;
+export default ChatWidget;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById("root-chat");
+const root = ReactDOM.createRoot(container);
+document.body.appendChild(container);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+// Render the chat widget component inside the container
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
